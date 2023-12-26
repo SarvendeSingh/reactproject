8e52639f0e34027e3b4f386010538704e57738ce
sarvender singh <sarvendersingh3@gmail.com>
2023-12-27 00:17:43 +0530

    Add initial project files

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {Container, Logo, LogoutBtn} from '../index'
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+function Header(){
+    const authStatus = useSelector((state) => state.auth.status)
+    return(
+       
+    )
+}
+
+export default Header;
